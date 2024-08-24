d54f72f3f2b3fcfaa5f61b366813590a14157458
Krishnachandran U <kriss52450@gmail.com>
2024-08-24 11:28:07 +0530

    implement basic training

--- _nn.py
+++ _nn.py
@@ -1,13 +1,17 @@
 from typing import List
 import random
-from _utils import dot
+from _utils import *
 class Layer:
     n_neurons: int
-    prev_layer: 'Layer' 
-    next_layer: 'Layer'
+    bias: float
+    act: str
 
-    def __init__(self, n_neurons: int):
+    def __init__(self, n_neurons: int, bias: float = 0, act: str = None):
         self.n_neurons = n_neurons
+        self.bias = bias
+        if act is not None and act not in acts:
+            raise ValueError("Invalid Activation function")
+        self.act = act
 
 class NeuralNet:
     layers: List[Layer]
@@ -17,10 +21,10 @@ class NeuralNet:
         self.layers = layers
         self._init_weights()
 
-    def _init_weights_2d(self, m: int, n: int) -> List[List[float]]: # make an n x m matrix - m: neurons in previous layer, n: neurons in next layer
-        weights_2d = [[0]*m for _ in range(n)]
-        for j in range(n):
-            for k in range(m):
+    def _init_weights_2d(self, m: int, n: int) -> List[List[float]]: #im not sure if this is correct
+        weights_2d = [[0]*n for _ in range(m)]
+        for j in range(m):
+            for k in range(n):
                 weights_2d[j][k] = random.uniform(-1, 1)
         return weights_2d
 
@@ -28,16 +32,39 @@ class NeuralNet:
         for i in range(len(self.layers) - 1):
             self.weights.append(self._init_weights_2d(self.layers[i].n_neurons, self.layers[i + 1].n_neurons))
 
-    def _feed_forward(self, x: List[float]) -> List[float]: 
-        outputs = x
+    def train(self, x: List[List[float]], y: List[List[float]], alpha: float, epochs: int) -> None:
+        if len(x) != len(y):
+            raise ValueError("Input and output count mismatch")
+        if not is_rectangular(x):
+            raise ValueError("Input matrix is not rectangular")
+        if not is_rectangular(y):
+            raise ValueError("Output matrix is not rectangular")
+
+        for i in range(len(x)):
+            for _ in range(epochs):
+                A = [x[i]]
+                A = self.feed_forward(A)
+                D = self.back_propagation(A, y[i])
+                self.update_weights(A, D, alpha)
+
+    def feed_forward(self, A: List[List[float]]) -> List[float]:
         for i in range(len(self.layers) - 1):
-            outputs = dot(outputs, self.weights[i])
-        return outputs
+            A.append(
+                dot(A[i], self.weights[i], self.layers[i + 1].act, self.layers[i + 1].bias)
+            )
+        return A
 
-    def train(self, x: List[float], y: List[float], learning_rate: float):
-        if len(x) != self.layers[0].n_neurons:
-            raise ValueError("Input layer size mismatch")
-        if len(y) != self.layers[-1].n_neurons:
-            raise ValueError("Output layer size mismatch")
+    def back_propagation(self, A: List[List[float]], y: List[float]):
+        err = sub(A[-1], y)**2
+        D = [err * acts[self.layers[-1].act]["d"](A[-1])]
 
-        # run a loop with feed forward and backpropagation 
+        for layer in range(len(self.layers) - 2, 0, -1):
+            delta = dot(D[0], transpose(self.weights[layer]))   
+            delta = delta * acts[self.layers[layer].act]["d"](A[layer])
+            D.insert(0, delta)
+
+        return D
+
+    def update_weights(self, A: List[List[float]], D: List[List[float]], alpha: float) -> None:
+        for i in range(len(self.weights)):
+            self.weights[i] += -alpha * dot(transpose(A[i]), D[i])
--- _utils.py
+++ _utils.py
@@ -1,20 +1,69 @@
 from typing import List
 from _err import MatrixDimensionError
+import math
 
-def dot(a: List[List[int]], b: List[List[int]]) -> List[List[int]]:
-    def is_valid(a: List[List[int]]) -> bool:
-        row_lengths_a = [len(row) for row in a]
-        if not all(length == row_lengths_a[0] for length in row_lengths_a): 
+tanh = lambda x: math.tanh(x)
+d_tanh = lambda output: 1 - output ** 2
+
+sigmoid = lambda x: 1 / (1 + math.exp(-x))
+d_sigmoid = lambda output: output * (1 - output)
+
+relu = lambda x: max(0, x)
+d_relu = lambda output: 1 if output > 0 else 0
+
+leaky_relu = lambda x: max(0.01 * x, x)
+d_leaky_relu = lambda output: 1 if output > 0 else 0.01
+
+acts = {
+    "tanh": {
+        "f": tanh,
+        "d": d_tanh
+    },
+    "sigmoid": {
+        "f": sigmoid,
+        "d": d_sigmoid
+    },
+    "relu": {
+        "f": relu,
+        "d": d_relu
+    },
+    "leaky_relu": {
+        "f": leaky_relu,
+        "d": d_leaky_relu
+    }
+}
+
+def dot(a: List[List[float]], b: List[List[float]], act: str = None, bias: float = 0) -> List[List[float]]:
+    def is_valid(a: List[List[int]]) -> None:
+        if not is_rectangular(a): 
             raise MatrixDimensionError("First matrix is not rectangular")
-        row_lengths_b = [len(row) for row in b]
-        if not all(length == row_lengths_b[0] for length in row_lengths_b):
+        if not is_rectangular(b):
             raise MatrixDimensionError("Second matrix is not rectangular")
         if len(a[0]) != len(b): 
             raise MatrixDimensionError("Matrix dimensions do not match")
+        if act is not None and act not in acts:
+            raise ValueError("Invalid Activation function")
+
+    def activate(x: float) -> float:
+        return x if act is None else acts[act]["f"](x)
 
     is_valid()
 
     try:
-        return [[sum(a[i][k] * b[k][j] for k in range(len(a[0]))) for j in range(len(b[0]))] for i in range(len(a))]
+        return [[activate(sum(a[i][k] * b[k][j] for k in range(len(a[0]))) for j in range(len(b[0])) + bias)] for i in range(len(a))]
     except Exception as e:
         raise ValueError("Matrix multiplication failed") from e
+
+def sub(a: List[List[float]], b: List[List[float]]) -> List[List[float]]:
+    if len(a) != len(b) or len(a[0]) != len(b[0]):
+        raise MatrixDimensionError("Matrix dimensions do not match")
+    return [[a[i][j] - b[i][j] for j in range(len(a[0]))] for i in range(len(a))]
+
+def is_rectangular(a: List[List[float]]) -> bool:
+    row_lengths = [len(row) for row in a]
+    return all(length == row_lengths[0] for length in row_lengths)
+
+def transpose(a: List[List[float]]) -> List[List[float]]:
+    if not is_rectangular(a):
+        raise ValueError("Matrix is not rectangular")
+    return [[a[j][i] for j in range(len(a))] for i in range(len(a[0]))]
